dde7aad248c98560c3c42ab15014608cf5389827
unknown <micaelpedrosa@gmail.com>
2015-11-19 11:18:13 +0000

    just a comment in SessionManager

--- src/main/java/eu/rethink/mn/component/SessionManager.java
+++ src/main/java/eu/rethink/mn/component/SessionManager.java
@@ -29,5 +29,9 @@ public class SessionManager implements IComponent {
 			register.unbind(msg.getFrom());
 			ctx.disconnect();
 		}
+		
+		//TODO: manage ping message to maintain the open connection?
+		//how to handle timeouts ?
+		//if(msg.getType().equals("ping")) {}
 	}
 }
