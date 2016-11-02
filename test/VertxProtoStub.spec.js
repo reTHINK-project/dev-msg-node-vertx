ec2573461271620e45271a770bbc324df5f9e46c
luistduarte <lduarte.suil@gmail.com>
2016-11-02 13:03:54 +0000

    added hyperty and object handlers again.. and new tests for reusage of address allocation

--- test/VertxProtoStub.spec.js
+++ test/VertxProtoStub.spec.js
@@ -321,4 +321,132 @@ describe('VertxProtoStub', function() {
     });
   });
 
+  it('hyperty registration with address reusage', function(done) {
+    let protoURL = 'hyperty-runtime://sp1/protostub/123';
+    let send;
+    let proto;
+
+    let seq = 0;
+    let firstURL;
+    let secondURL;
+
+    let bus = {
+      postMessage: (msg) => {
+        if (seq === 0) {
+          expect(msg).to.eql({
+            type: 'update', from: protoURL, to: 'hyperty-runtime://sp1/protostub/123/status',
+            body: {value: 'connected'}
+          });
+        }
+
+        if (seq === 1) {
+          /*expect something like -> {
+            id: 1, type: 'response', from: 'domain://msg-node.ua.pt/hyperty-address-allocation', to: 'runtime:/alice/registry/allocation',
+            body: {code: 200, allocated: ['hyperty-instance://ua.pt/fbf7dc26-ff4f-454f-961e-22edda927561', 'hyperty-instance://ua.pt/6e8f126b-1c56-4525-9a38-5dcd340194da']}
+          }*/
+          expect(msg).to.eql({id: 1, type: 'response', from: 'domain://msg-node.ua.pt/address-allocation', to: 'runtime:/alice/registry/allocation', body: msg.body});
+          expect(msg.body.code).to.eql(200);
+          expect(msg.body.value.allocated).to.have.length(2);
+
+          firstURL = msg.body.value.allocated[0];
+          secondURL = msg.body.value.allocated[1];
+
+          send({id: 1, type: 'ping', from: firstURL, to: secondURL});
+        }
+
+        if (seq === 2) {
+          expect(msg).to.eql({
+            id: 1, type: 'ping', from: firstURL, to: secondURL,
+            body: { via: protoURL }
+          });
+
+          proto.disconnect();
+          done();
+        }
+
+        seq++;
+      },
+
+      addListener: (url, callback) => {
+        send = callback;
+      }
+    };
+
+    let config = {
+      url: 'wss://msg-node.ua.pt:9090/ws',
+      runtimeURL: 'runtime:/alice2'
+    };
+
+    proto = activate(protoURL, bus, config).instance;
+
+    send({
+      id: 1, type: 'create', from: 'runtime:/alice/registry/allocation', to: 'domain://msg-node.ua.pt/address-allocation',
+      body: { value: { number: 2 } }
+    });
+  });
+
+  it('object registration with address reusage', function(done) {
+    let protoURL = 'hyperty-runtime://sp1/protostub/123';
+    let send;
+    let proto;
+
+    let seq = 0;
+    let url;
+    let urlChildren;
+
+    let bus = {
+      postMessage: (msg) => {
+        if (seq === 0) {
+          expect(msg).to.eql({
+            type: 'update', from: 'hyperty-runtime://sp1/protostub/123', to: 'hyperty-runtime://sp1/protostub/123/status',
+            body: {value: 'connected'}
+          });
+        }
+
+        if (seq === 1) {
+          /*expect something like -> {
+            id: 1, type: 'response', from: 'domain://msg-node.ua.pt/hyperty-address-allocation', to: 'runtime:/alice/registry/allocation',
+            body: {code: 200, allocated: ['hyperty-instance://ua.pt/fbf7dc26-ff4f-454f-961e-22edda927561', 'hyperty-instance://ua.pt/6e8f126b-1c56-4525-9a38-5dcd340194da']}
+          }*/
+          expect(msg).to.eql({id: 1, type: 'response', from: 'domain://msg-node.ua.pt/address-allocation', to: 'runtime:/alice/registry/allocation', body: msg.body});
+          expect(msg.body.code).to.eql(200);
+          expect(msg.body.value.allocated).to.have.length(1);
+
+          url = msg.body.value.allocated[0];
+          urlChildren = url;
+
+          send({id: 1, type: 'ping', from: url, to: urlChildren});
+        }
+
+        if (seq === 2) {
+          expect(msg).to.eql({
+            id: 1, type: 'ping', from: url, to: urlChildren,
+            body: { via: protoURL }
+          });
+
+          proto.disconnect();
+          done();
+        }
+
+        seq++;
+      },
+
+      addListener: (url, callback) => {
+        send = callback;
+      }
+    };
+
+    let config = {
+      url: 'wss://msg-node.ua.pt:9090/ws',
+      runtimeURL: 'runtime:/alice2'
+    };
+
+    proto = activate('hyperty-runtime://sp1/protostub/123', bus, config).instance;
+
+    send({
+      id: 1, type: 'create', from: 'runtime:/alice/registry/allocation', to: 'domain://msg-node.ua.pt/address-allocation',
+      body: { scheme: 'fake', childrenResources: ['message'], value: { number: 1 } }
+    });
+  });
+
 });
